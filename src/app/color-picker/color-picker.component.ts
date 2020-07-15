6dacf7eda17080c00ab02176d27a28dc48f117fe
j-agudeloariza <j-agudelo.ariza@hotmail.com>
2020-07-15 14:09:44 -0500

    List Movie

---
+++ src/app/color-picker/color-picker.component.ts
@@ -0,0 +1,22 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-color-picker',
+  templateUrl: './color-picker.component.html',
+  styleUrls: ['./color-picker.component.scss']
+})
+export class ColorPickerComponent implements OnInit {
+  @Input() colorOptions: string[];
+  @Input() initialColor: string;
+  selectedColor : string;
+
+
+  ngOnInit(): void {
+  }
+
+  colores(e){
+   this.selectedColor = e.style.backgroundColor;
+  }
+
+
+}
